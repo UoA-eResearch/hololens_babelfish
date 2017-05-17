6b05451487828920e6b0fdd3456de7abdf8c1370
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-05-17 16:20:56 +1200

    init

---
+++ Assets/MicrophoneManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class MicrophoneManager : MonoBehaviour
+{
+	[Tooltip("A text area for the recognizer to display the recognized strings.")]
+	public Text DictationDisplay;
+	private Dictionary<string, string> supportedTranslationLanguages;
+	private string target_lang = "FR";
+	private string target_langName = "French";
+
+	private DictationRecognizer dictationRecognizer;
+
+	// Use this string to cache the text currently displayed in the text box.
+	private string englishS = "English: ";
+	private string target_langS = "French: ";
+
+	void WriteOut(string hypothesis = "")
+	{
+		if (hypothesis.Length > 0)
+		{
+			DictationDisplay.text = englishS + " " + hypothesis + "..." + "\n" + target_langS;
+		} else
+		{
+			DictationDisplay.text = englishS + "\n" + target_langS;
+		}
+	}
+
+	void Start()
+	{
+		Debug.Log("start");
+		LoadSupportedLanguages();
+		//SetTargetLang("german");
+		StartCoroutine(TranslateText("Waiting for speech"));
+		dictationRecognizer = new DictationRecognizer();
+
+		dictationRecognizer.DictationHypothesis += (text) =>
+			{
+				Debug.LogFormat("Dictation hypothesis: {0}", text);
+				WriteOut(text);
+			};
+
+
+		dictationRecognizer.DictationResult += (text, confidence) =>
+			{
+				Debug.LogFormat("Dictation result: {0}", text);
+				var bits = text.Split(' ');
+				if (bits.Length == 3 && bits[0] == "translate")
+				{
+					string name = bits[2];
+					SetTargetLang(name);
+				}
+				else
+				{
+					englishS = "english: " + text + ". ";
+					WriteOut();
+					StartCoroutine(TranslateText(text));
+				}
+			};
+
+		dictationRecognizer.DictationComplete += (completionCause) =>
+			{
+				Debug.LogErrorFormat("Dictation completed: {0}.", completionCause);
+				dictationRecognizer.Start();
+			};
+
+		dictationRecognizer.DictationError += (error, hresult) =>
+			{
+				Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+			};
+
+		dictationRecognizer.Start();
+	}
+
+	void LoadSupportedLanguages()
+	{
+		var supportedTranslationLanguagesA = Resources.Load<TextAsset>("google_translate_supported_languages").text.Split('\n');
+		supportedTranslationLanguages = new Dictionary<string, string>();
+		foreach (var line in supportedTranslationLanguagesA)
+		{
+			var bits = line.Trim().Split();
+			var code = bits[bits.Length - 1].Trim();
+			var name = line.Replace(code, "").ToLower().Trim();
+			//Debug.Log(name);
+			supportedTranslationLanguages[name] = code;
+		}
+	}
+
+	void SetTargetLang(string name)
+	{
+		if (supportedTranslationLanguages.ContainsKey(name))
+		{
+			target_langName = name;
+			target_lang = supportedTranslationLanguages[name];
+			englishS = "Set target lang to " + target_lang;
+			Debug.Log(englishS);
+			target_langS = name + ": ";
+		} else
+		{
+			englishS = name + " is not a supported language";
+			Debug.Log(englishS);
+			target_langS = name + ": ";
+		}
+		WriteOut();
+	}
+
+	IEnumerator<object> TranslateText(string text)
+	{
+
+		string url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl=EN&tl={0}&dt=t&q={1}", target_lang, WWW.EscapeURL(text));
+		Debug.Log(url);
+		WWW www = new WWW(url);
+		yield return www;
+		JSONObject j = new JSONObject(www.text);
+		string result = j[0][0][0].str;
+		Debug.Log("Translation result: " + result);
+		target_langS = target_langName + ": " + result;
+		WriteOut();
+	}
+
+	void OnApplicationQuit()
+	{
+		Debug.Log("Application ending after " + Time.time + " seconds");
+		dictationRecognizer.Stop();
+		dictationRecognizer.Dispose();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# hololens_bablefish
+Translation app for the hololens. Uses the dictation api to translate spoken English (by the user, or someone they're facing) into text, which is then translated into the chosen language. Defaults to French. Say the phrase "translate to [language_name]" to change the output language. For example, "translate to german".
+
+![Screenshot](20170517_161634_HoloLens.jpg)
